1ea227f91967bff02c61ea16f2bc13ba90751c88
Dmitrii Fufachev <Dima@eduroam.nat.ru.is>
2018-11-13 13:25:11 +0000

    added comment to .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
 
     // Comment
     "extends": "airbnb-base",
+    // To allow document and fetch be recognised as
+    // global variables
     "env": {
         "browser": true
     }
